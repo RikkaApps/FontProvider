a123976bba83d788711451ae4ad4cf3d7564a2f1
RikkaW <rikka@shizuku.moe>
2017-10-11 18:42:38 +0800

    app: introduce Fabric

--- app/src/main/java/moe/shizuku/fontprovider/FontProviderApplication.java
+++ app/src/main/java/moe/shizuku/fontprovider/FontProviderApplication.java
@@ -2,6 +2,10 @@ package moe.shizuku.fontprovider;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
+
+import io.fabric.sdk.android.Fabric;
 import moe.shizuku.fontprovider.font.FontManager;
 
 /**
@@ -13,6 +17,12 @@ public class FontProviderApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Crashlytics crashlyticsKit = new Crashlytics.Builder()
+                .core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
+                .build();
+
+        Fabric.with(this, crashlyticsKit);
 
         FontProviderSettings.init(this);
         FontManager.init(this);
